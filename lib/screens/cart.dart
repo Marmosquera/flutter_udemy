cfc33467499e662d22532e35f6b731a54e6691a4
Marcelo Mosquera <marcelo@stampit.co>
2021-07-02 11:49:29 -0300

    cart full

--- lib/screens/cart.dart
+++ lib/screens/cart.dart
@@ -1,9 +1,86 @@
 import 'package:flutter/material.dart';
 import 'package:udemy_course/screens/cart_empty.dart';
+import 'package:udemy_course/screens/cart_full.dart';
 
 class CartScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: CartEmpty());
+    List products = [];
+    return products.isNotEmpty
+        ? Scaffold(body: CartEmpty())
+        : Scaffold(
+            appBar: AppBar(
+              title: Text('Cart Items Count'),
+              actions: [
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.delete),
+                )
+              ],
+            ),
+            bottomSheet: checkoutSection(context),
+            body: Container(
+              margin: EdgeInsets.only(bottom: 60),
+              child: ListView.builder(
+                  itemCount: 5,
+                  itemBuilder: (BuildContext ctx, int index) {
+                    return CartFull();
+                  }),
+            ));
+  }
+
+  Widget checkoutSection(BuildContext ctx) {
+    return Container(
+        decoration: BoxDecoration(
+          border: Border(
+            top: BorderSide(color: Colors.grey, width: 0.5),
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            /// mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Expanded(
+                flex: 2,
+                child: Material(
+                  borderRadius: BorderRadius.circular(30),
+                  color: Colors.red,
+                  child: InkWell(
+                    borderRadius: BorderRadius.circular(30),
+                    onTap: () {},
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        'Checkout',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            color: Theme.of(ctx).textSelectionColor,
+                            fontSize: 18,
+                            fontWeight: FontWeight.w600),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Spacer(),
+              Text(
+                'Total:',
+                style: TextStyle(
+                    color: Theme.of(ctx).textSelectionTheme.selectionColor,
+                    fontSize: 18,
+                    fontWeight: FontWeight.w600),
+              ),
+              Text(
+                'US \$179.0',
+                //textAlign: TextAlign.center,
+                style: TextStyle(
+                    color: Colors.blue,
+                    fontSize: 18,
+                    fontWeight: FontWeight.w500),
+              ),
+            ],
+          ),
+        ));
   }
 }
