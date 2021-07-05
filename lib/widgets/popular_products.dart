dcbb7d076abe8f97ab689f631e84085b2e229668
Marcelo Mosquera <marcelo@stampit.co>
2021-07-05 15:22:56 -0300

    fix popular ontap

--- lib/widgets/popular_products.dart
+++ lib/widgets/popular_products.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:udemy_course/screens/product_detail.dart';
 import '/models/product.dart';
 
 class PopularProducts extends StatelessWidget {
@@ -18,99 +19,113 @@ class PopularProducts extends StatelessWidget {
               bottomLeft: Radius.circular(10),
               bottomRight: Radius.circular(10)),
         ),
-        child: Column(
-          children: [
-            Stack(
+        child: Material(
+          color: Colors.transparent,
+          child: InkWell(
+            borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(
+                10.0,
+              ),
+              bottomRight: Radius.circular(10.0),
+            ),
+            onTap: () => Navigator.pushNamed(context, ProductDetail.routeName,
+                arguments: _product.id),
+            child: Column(
               children: [
-                Container(
-                  height: 170,
-                  decoration: BoxDecoration(
-                      image: DecorationImage(
-                          image: NetworkImage(_product.imageUrl),
-                          fit: BoxFit.contain)),
-                ),
-                Positioned(
-                  right: 10,
-                  top: 8,
-                  child: Icon(Icons.check, color: Colors.red),
-                ),
-                Positioned(
-                  right: 10,
-                  top: 8,
-                  child: Icon(
-                    Icons.camera,
-                    color: Colors.white,
-                  ),
-                ),
-                Positioned(
-                  right: 12,
-                  bottom: 32.0,
-                  child: Container(
-                    padding: EdgeInsets.all(10.0),
-                    color: Theme.of(context).backgroundColor,
-                    child: Text(
-                      '\$ ${_product.price}',
-                      style: TextStyle(
-                        color:
-                            Theme.of(context).textSelectionTheme.selectionColor,
+                Stack(
+                  children: [
+                    Container(
+                      height: 170,
+                      decoration: BoxDecoration(
+                          image: DecorationImage(
+                              image: NetworkImage(_product.imageUrl),
+                              fit: BoxFit.contain)),
+                    ),
+                    Positioned(
+                      right: 10,
+                      top: 8,
+                      child: Icon(Icons.check, color: Colors.red),
+                    ),
+                    Positioned(
+                      right: 10,
+                      top: 8,
+                      child: Icon(
+                        Icons.camera,
+                        color: Colors.white,
                       ),
                     ),
-                  ),
-                )
-              ],
-            ),
-            Container(
-              padding: EdgeInsets.all(8.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    _product.title,
-                    maxLines: 1,
-                    style:
-                        TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
-                  ),
-                  Row(
-                    //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Expanded(
-                        flex: 5,
+                    Positioned(
+                      right: 12,
+                      bottom: 32.0,
+                      child: Container(
+                        padding: EdgeInsets.all(10.0),
+                        color: Theme.of(context).backgroundColor,
                         child: Text(
-                          _product.description,
-                          maxLines: 2,
-                          overflow: TextOverflow.ellipsis,
+                          '\$ ${_product.price}',
                           style: TextStyle(
-                            fontSize: 15.0,
-                            fontWeight: FontWeight.w500,
-                            color: Colors.grey[800],
+                            color: Theme.of(context)
+                                .textSelectionTheme
+                                .selectionColor,
                           ),
                         ),
                       ),
-                      Spacer(),
-                      Expanded(
-                        flex: 1,
-                        child: Material(
-                          color: Colors.transparent,
-                          child: InkWell(
-                            onTap: () {},
-                            borderRadius: BorderRadius.circular(30.0),
-                            child: Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Icon(
-                                Icons.check_box,
-                                size: 25,
-                                color: Colors.black,
+                    )
+                  ],
+                ),
+                Container(
+                  padding: EdgeInsets.all(8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        _product.title,
+                        maxLines: 1,
+                        style: TextStyle(
+                            fontSize: 18.0, fontWeight: FontWeight.bold),
+                      ),
+                      Row(
+                        //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Expanded(
+                            flex: 5,
+                            child: Text(
+                              _product.description,
+                              maxLines: 2,
+                              overflow: TextOverflow.ellipsis,
+                              style: TextStyle(
+                                fontSize: 15.0,
+                                fontWeight: FontWeight.w500,
+                                color: Colors.grey[800],
                               ),
                             ),
                           ),
-                        ),
-                      )
+                          Spacer(),
+                          Expanded(
+                            flex: 1,
+                            child: Material(
+                              color: Colors.transparent,
+                              child: InkWell(
+                                onTap: () {},
+                                borderRadius: BorderRadius.circular(30.0),
+                                child: Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Icon(
+                                    Icons.check_box,
+                                    size: 25,
+                                    color: Colors.black,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
                     ],
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
